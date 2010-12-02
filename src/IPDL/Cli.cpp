283761067ed9b7f402a29df44558958768769f5b
James Fisher <jameshfisher@gmail.com>
2010-12-02 14:26:49 +0000

    Switched h and cpp -- stupid

--- src/IPDL/Cli.cpp
+++ src/IPDL/Cli.cpp
@@ -1,51 +1,110 @@
-using System;
-using System.IO;
-using Mono.Options;
-using System.Reflection;
-
 #include "Cli.h"
 
 namespace IPDL {
-  class Cli {
 
-	private: // Data members
-		       // ------------
+  Cli::Cli(Downloader &downloader) {
+    this->downloader   = downloader;
+    this->succeeded    = true;
+    this->quiet        = false;
+  }
 
-    Downloader &downloader;
-    bool succeeded, quiet;
-    string lastOutput = "";
+  Cli::Cli() : this(new Downloader()) {}
+
+  void Cli::Run(string[] args) {
+    bool showHelp    = false;
+    bool showVersion = false;
+    var opts = new OptionSet(){
+      {"d=|download-path=", v => Directory.SetCurrentDirectory(v)},
+      {"v|version",         v => showVersion = true},
+      {"q|quiet",           v => quiet = true},
+      {"h|help",            v => showHelp = true}
+    };
+    var identifiers = opts.Parse(args);
+    if (showVersion) {
+      ShowVersion();
+      return;
+    }
+    if (showHelp || identifiers.Count == 0) {
+      ShowHelp();
+      return;
+    }
+    foreach (var identifier in identifiers) {
+      Download(identifier);
+    }
+  }
 
 
-	public: // Public methods
-		      // --------------
+  bool Cli::get_succeeded() {
+    return this.succeeded;
+  }
 
-		// Constructors
-    Cli(Downloader &downloader);
-		Cli();
+  void Cli::ShowVersion() {
+    var details = Assembly.GetName();
+    Console.WriteLine("{0} version {1}", details.Name, details.Version);
+  }
 
-    void Run(string[] args);
+  void Cli::ShowHelp() {
+    Console.WriteLine((new StreamReader(Assembly.GetManifestResourceStream("help.txt"))).ReadToEnd());
+  }
 
-    bool get_succeeded();
-		// Just get the value of private member `succeeded`
+  Assembly Cli::get_Assembly() {
+    return Assembly.GetExecutingAssembly();
+  }
 
+  void Cli::Download(string identifier) {
+    var pid = Util.ExtractPid(identifier);
+    if (pid == null) {
+      if (!this.quiet) {
+	Console.WriteLine("ERROR: {0} is not recognised as a programme ID", identifier);
+      }
+      this.succeeded = false;
+      return;
+    }
+    downloader.Download(pid, DownloadStart, DownloadProgress, DownloadEnd);
+  }
 
-	private: // Private methods
-           // ---------------
+  void Cli::DownloadStart(string filename) {
+    if (this.quiet) return;
 
-    void ShowVersion();
+    Console.WriteLine("Downloading: {0}", filename);
+  }
 
-    void ShowHelp();
+  void Cli::DownloadProgress(int bytesDownloaded, int total) {
+    if (this.quiet) return;
 
-    Assembly get_Assembly();
+    string output = String.Format("{1:0.0}% of {0}",
+				  Util.SIFormat(total, "B"),
+				  (bytesDownloaded * 100.0) / total);
+    if (output != lastOutput) {
+#if !TEST_BUILD
+      Console.CursorLeft = 0;
+#endif
+      Console.Write(output);
+      lastOutput = output;
+    }
+  }
 
-    void Download(string identifier);
-
-    void DownloadStart(string filename);
-
-    void DownloadProgress(int bytesDownloaded, int total);
-
-    void DownloadEnd(DownloadStatus status, string message);
-
-  }; // class Cli
-
-} // namespace IPDL
+  void Cli::DownloadEnd(DownloadStatus status, string message) {
+    string output = "";
+    switch (status) {
+    case DownloadStatus.Complete:
+      output = String.Format("SUCCESS: Downloaded to {0}", message);
+      break;
+    case DownloadStatus.Incomplete:
+      output = String.Format("FAILED: Incomplete download saved as {0}", message);
+      this.succeeded = false;
+      break;
+    case DownloadStatus.AlreadyExists:
+      output = String.Format("SKIP: File exists: {0}", message);
+      break;
+    case DownloadStatus.Unavailable:
+      output = String.Format("ERROR: {0} is currently unavailable", message);
+      this.succeeded = false;
+      break;
+    }
+    if (!this.quiet) {
+      Console.WriteLine();
+      Console.WriteLine(output);
+    }
+  }
+}
