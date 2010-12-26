c2e7b4e1fc659538100b87bb1a1e9394b6567954
James Harrison Fisher <jameshfisher@gmail.com>
2010-12-26 17:26:27 +0000

    Added doxygen documentation

--- src/IPDL/Downloader.h
+++ src/IPDL/Downloader.h
@@ -12,19 +12,39 @@ namespace IPDL {
 
   enum DownloadStatus { Complete, Incomplete, AlreadyExists, Unavailable, Error };
 
+  /**
+   * A downloader of a single file,
+   * which sends notifications of progress via callbacks.
+   */
   class Downloader {
 
   private: // Private members
 
+    /**
+     * A container for cookies.
+     * Contains many cookies.
+     */
     CookieContainer cookies;
+
     static int BufferSize = 0x10000;
 
   public: // Public methods
 
-    // Constructors
+    /**
+     * A constructor.
+     * Create a downloader with no arguments:
+     * call Download() to use it on a single file.
+     */
     Downloader();
 
-    // THE BIG ONE
+    /**
+     * Download a specified BBC program to a file.
+     *
+     * @param pid      the unique identifier of the program to download
+     * @param atStart  callback to be notified that the download has started
+     * @param progress callback to be notified of a change in how much has downloaded
+     * @param atEnd    callback to be notified that the download has finished
+     */
     void Download(string pid,
 									void (atStart*)(string filename),
 									void (progress*)(int bytesFetched, int bytesTotal),
